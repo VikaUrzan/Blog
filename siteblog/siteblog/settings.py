2f64a91bd5e1df6773684005504efb07c0ed0fed
ШАР <vikysya163945@gmail.com>
2020-08-18 16:17:46 +0400

    [develop] cache home

--- siteblog/siteblog/settings.py
+++ siteblog/siteblog/settings.py
@@ -201,3 +201,11 @@ CKEDITOR_CONFIGS = {
         ]),
     }
 }
+
+
+CACHES = {
+    'default': {
+        'BACKEND': 'django.core.cache.backends.filebased.FileBasedCache',
+        'LOCATION': os.path.join(BASE_DIR, 'siteblog/django_cache'),
+    }
+}
