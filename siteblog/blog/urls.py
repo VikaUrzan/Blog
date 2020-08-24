52adfa896a4c5f8ea014fc04a3beeab08322ca0e
ШАР <vikysya163945@gmail.com>
2020-08-24 17:30:41 +0400

    [develop] separate article

--- siteblog/blog/urls.py
+++ siteblog/blog/urls.py
@@ -3,6 +3,6 @@ from .views import *
 
 urlpatterns = [
     path('', Home.as_view(), name='home'),
-    path('category/<str:slug>/', get_category, name='category'),
-    path('post/<str:slug>', get_post, name = 'post')
+    path('category/<str:slug>/', PostByCategory.as_view(), name='category'),
+    path('post/<str:slug>', GetPost.as_view(), name='post'),
 ]
--- siteblog/blog/views.py
+++ siteblog/blog/views.py
@@ -1,15 +1,16 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.views.generic import ListView, DetailView
-from .models import Post
+from .models import Post, Category
 from django.core.paginator import Paginator
+from django.db.models import F
 
 
 class Home(ListView):
     model = Post
     template_name = 'blog/home_blog.html'
     context_object_name = 'posts'
-    paginate_by = 2
+    paginate_by = 3
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
@@ -17,10 +18,28 @@ class Home(ListView):
         return context
 
 
-def get_category(request, slug):
-    return render(request, 'blog/category.html')
+class PostByCategory(ListView):
+    template_name = 'blog/home_blog.html'
+    context_object_name = 'posts'
+    paginate_by = 1
+
+    def get_queryset(self):
+        return Post.objects.filter(category__slug = self.kwargs['slug'])
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = Category.objects.get(slug =self.kwargs['slug'] )
+        return context
 
 
-def get_post(request, slug):
-    return render(request, 'blog/home_blog.html')
+class GetPost(DetailView):
+    model = Post
+    template_name = 'blog/single.html'
+    context_object_name = 'post'
 
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        self.object.views = F('views') + 1
+        self.object.save()
+        self.object.refresh_from_db()
+        return context
